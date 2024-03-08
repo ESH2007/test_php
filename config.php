92044cfbc3831601dacf17fd32d98e195ad0b9d3
Emilio Sanchez <milaroco3004@gmail.com>
2024-03-07 20:51:17 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
-$servername = getenv('MYSQLPORT') ?: "localhost";
+$servername = getenv('MYSQLHOST') ?: "localhost";
 $username = getenv('MYSQLUSER') ?: "root";
 $password = getenv('MYSQL_ROOT_PASSWORD') ?: "admin";
 $dbname = getenv('MYSQLDATABASE') ?: "railway";
+$dbport = getenv('MYSQLPORT');
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $dbport);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
