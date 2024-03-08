e66c2701a9dfef0eb1e1df49b95415c10720a8d8
Emilio Sanchez <milaroco3004@gmail.com>
2024-03-07 19:31:34 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = getenv('DB_SERVER_NAME') ?: "localhost";
-$username = getenv('DB_USERNAME') ?: "your_username";
-$password = getenv('DB_PASSWORD') ?: "your_password";
-$dbname = getenv('DB_NAME') ?: "user_authentication";
+$servername = getenv('MYSQLHOST') ?: "localhost";
+$username = getenv('MYSQLUSER') ?: "root";
+$password = getenv('MYSQL_ROOT_PASSWORD') ?: "2B6GcH6HfHAaH2gGfG123A4CD62544g1";
+$dbname = getenv('MYSQLDATABASE') ?: "railway";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
